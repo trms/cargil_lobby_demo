e5d3a32e29de1cc0c6a0b33601a9c19990d343b6
Seth Phillips <seth.phillips@trms.com>
2015-04-10 10:07:00 -0500

    add migration for description to be text

---
+++ database/migrations/2015_04_10_145705_UpdateDescriptionToText.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateDescriptionToText extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('listings', function($table)
+		{
+		    $table->text('description')->change();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('listings', function($table)
+		{
+		    $table->string('description')->change();
+		});
+	}
+
+}
